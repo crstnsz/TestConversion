f67f1304973e470e684b80e8c6d5b73acea06135
Cristiano Souza <cristianos@docspider.com.br>
2024-10-31 17:20:00 -0300

    Converter testes

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+using static System.IO.File;
+
+// See https://aka.ms/new-console-template for more information
+var test = ReadAllText("Code.txt");
+
+var shouldIndex = test.IndexOf("Should");
+var endNameIndex = test.IndexOf("(", shouldIndex);
+
+
+for (var i = endNameIndex; i < test.Length; i++)
+{
+    if (test[i] == '=')
+    {
+        Write("Test(\"");
+        Write(test[shouldIndex..(endNameIndex)].Replace("_", " "));
+        Write(@""", async fixture");
+        WriteLine(test[(endNameIndex + 2)..].Replace("_fixture", "fixture").Replace(";", ");"));
+        break;
+    }
+
+    if (test[i] == '{')
+    {
+        Write("Test(\"");
+        Write(test[shouldIndex..(endNameIndex)].Replace("_", " "));
+        WriteLine(@""", async fixture");
+        WriteLine("                    =>");
+        Write(test[(i)..].Replace("_fixture", "fixture"));
+        Write(");");
+        break;
+    }
+}
+
+
+
+
+
+
